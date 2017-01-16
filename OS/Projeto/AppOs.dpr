f9a366db72886c6ee7156b93e03b31a20391b79a
PedroPaiv <efficax.pedro@gmail.com>
2017-01-16 16:51:06 -0400

    16/01/2017

--- OS/Projeto/AppOs.dpr
+++ OS/Projeto/AppOs.dpr
@@ -4,10 +4,10 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   frmLogin in '..\Visual\frmLogin.pas' {Login},
-  ufrmMenu in '..\Visual\ufrmMenu.pas' {frmMenu},
+  ufrmMenu in '..\Visual\ufrmMenu.pas' {frmMain},
   frmCadTec in '..\Visual\frmCadTec.pas' {CadTec},
   frmCadUser in '..\Visual\frmCadUser.pas' {CadUser},
-  frmCadCli in '..\Visual\frmCadCli.pas' {CadCli},
+  ufrmCadCli in '..\Visual\ufrmCadCli.pas' {frmCadCli},
   UPrinc in '..\Regras\UPrinc.pas',
   frmNDefOS in '..\Visual\frmNDefOS.pas',
   frmNDuvOS in '..\Visual\frmNDuvOS.pas' {NDuvOS},
@@ -17,6 +17,6 @@ uses
 
 begin
   Application.Initialize;
-  Application.CreateForm(TfrmMenu, frmMenu);
+  Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
 end.
